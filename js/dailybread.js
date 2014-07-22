b95abd0cddcd23fa642ad9253751bb1e1aa5ceff
iwakuracity <it@city.iwakura.lg.jp>
2014-07-22 14:21:04 +0900

    Update dailybread.js

--- js/dailybread.js
+++ js/dailybread.js
@@ -194,8 +194,8 @@ OpenSpending.DailyBread = function (elem) {
       var iconUrl, paper;
       iconUrl = $(e).data('svg-url');
       paper = Raphael(e, iconRad+iconRad,iconRad+iconRad+5);
-      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#830242', stroke: 'none' });
-      paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: 'none', stroke: '#eee', opacity: .8, 'stroke-dasharray': '- ' });
+      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#7FFF00', stroke: 'none' });
+      paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: '#7FFF00', stroke: '#eee', opacity: .8, 'stroke-dasharray': '- ' });
       $.get(iconUrl, function(svg) {
         if (typeof(svg) == "string") {
           svg = $(svg);
