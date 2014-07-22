f2fd252503743c9a7927197f384071481f8a1d09
iwakuracity <it@city.iwakura.lg.jp>
2014-07-22 18:56:51 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,7 +18,7 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/unemployment.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
   '1-1': { icon: 'icons/unemployment.svg', color: '#C75746', bcolor: '#935B3B' },
   '2': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-1': { icon: 'icons/media.svg', color: '#C75746', bcolor: '#0AB971' },
