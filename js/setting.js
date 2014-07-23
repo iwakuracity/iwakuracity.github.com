430bda53ae2a58fd8d4256c6adec516e8cec2b78
iwakuracity <it@city.iwakura.lg.jp>
2014-07-23 11:18:09 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,17 +18,17 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '11': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
-  '28': { icon: 'icons/c_syoubou_bousai.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
+  '3': { icon: 'icons/c_syoubou_bousai.svg', color: '#C75746', bcolor: '#935B3B' },
   '2': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-1': { icon: 'icons/media.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-2': { icon: 'icons/media.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-3': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-4': { icon: 'icons/secondary-lower.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-5': { icon: 'icons/secondary-upper.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-6': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-7': { icon: 'icons/secondary-upper.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-8': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' }
+  '4': { icon: 'icons/media.svg', color: '#C75746', bcolor: '#0AB971' },
+  '5': { icon: 'icons/media.svg', color: '#C75746', bcolor: '#0AB971' },
+  '6': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' },
+  '7': { icon: 'icons/secondary-lower.svg', color: '#C75746', bcolor: '#0AB971' },
+  '8': { icon: 'icons/secondary-upper.svg', color: '#C75746', bcolor: '#0AB971' },
+  '9': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' },
+  '10': { icon: 'icons/secondary-upper.svg', color: '#C75746', bcolor: '#0AB971' },
+  '11': { icon: 'icons/social-systems.svg', color: '#C75746', bcolor: '#0AB971' }
  
 };
 
